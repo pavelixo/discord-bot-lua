7ecf83fef4944d898ca9208de924b4b3ebcf4053
pavelixo <trashxindoragon87@gmail.com>
2024-06-26 18:45:29 -0300

    initial

---
+++ package.lua
@@ -0,0 +1,17 @@
+  return {
+    name = "discord-bot-lua",
+    version = "0.0.1",
+    description = "Discord bot using Lua and Discordia as an API wrapper.",
+    tags = { "lua", "lit", "luvit" },
+    license = "MIT",
+    author = { name = "pavelixo" },
+    homepage = "https://github.com/pavelixo/discord-bot-lua",
+    dependencies = {
+      "SinisterRectus/discordia@2.11.2"
+    },
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
+  
